5575542c11281a205f420d96053aaa3e5abc7eaf
hogang <hyogangster1@gmail.com>
2023-10-08 15:07:47 +0900

    구구단 구현 및 리팩토링 과제

---
+++ Gugudan/Gugudan/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Gugudan
+//
+//  Created by hogang on 2023/10/08.
+//
+
+import Foundation
+
+/*
+구구단을 리팩토링하자.
+ 1. 스크립트 버전으로 5단
+ 2. 중첩 루프 버전으로 1단 ~ 9단
+ 3. 함수로 바꿔서
+ 4. 구조체
+ 5. 클래스로
+ 6. Functional Programming 형태로
+ 7. 깃허브에 올리고 코드 리뷰 요청하기
+ */
