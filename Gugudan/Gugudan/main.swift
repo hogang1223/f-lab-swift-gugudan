9f25add83d6e28ccf4e5f4b02abd8777b3cf36e8
hogang <hyogangster1@gmail.com>
2023-10-09 21:51:05 +0900

    3. 함수로 바꿔서

--- Gugudan/Gugudan/main.swift
+++ Gugudan/Gugudan/main.swift
@@ -17,3 +17,40 @@ import Foundation
  6. Functional Programming 형태로
  7. 깃허브에 올리고 코드 리뷰 요청하기
  */
+
+
+// 3. 함수로 바꿔서
+
+// 입력받아서 출력
+func outputGugudanWithReadLine() {
+    print("몇 단을 출력할까요? (숫자만 입력해주세요)")
+    let readLine = readLine()
+    guard let column = readLine, Int(column) != nil else {
+        print("숫자만 입력해주세요.")
+        return
+    }
+    gugudan(column: Int(column)!)
+}
+
+func gugudan(column: Int) {
+    for row in 1...9 {
+        gugudan(column: column, row: row)
+    }
+}
+
+func gugudan(column: Int, row: Int) {
+    print("\(column) X \(row) = \(column * row)")
+}
+
+func gugudan() {
+    var column = 1
+    var row = 1
+    while column < 10 {
+        gugudan(column: column, row: row)
+        row += 1
+        if row > 9 {
+            row = 1
+            column += 1
+        }
+    }
+}
