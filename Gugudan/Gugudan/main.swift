cf68739941975d47c7d5962b3ba9df5103cbb6ed
hogang <hyogangster1@gmail.com>
2023-10-13 18:47:51 +0900

    함수형프로그래밍 형태로 구구단

--- Gugudan/Gugudan/main.swift
+++ Gugudan/Gugudan/main.swift
@@ -17,3 +17,23 @@ import Foundation
  6. Functional Programming 형태로
  7. 깃허브에 올리고 코드 리뷰 요청하기
  */
+
+//  6. Functional Programming 형태로
+
+func checkMax(num: Int, max: Int) -> Bool {
+    return num < max
+}
+
+func gugudan(start: Int, end: Int) {
+    func outputgugudan(column: Int, row: Int, checkMax: (Int, Int) -> Bool) {
+        print("\(column) X \(row) = \(column * row)")
+        if checkMax(row, end) {
+            outputgugudan(column: column, row: row + 1, checkMax: checkMax)
+        } else if checkMax(column, end) {
+            outputgugudan(column: column + 1, row: start, checkMax: checkMax)
+        }
+    }
+    outputgugudan(column: start, row: start, checkMax:checkMax(num:max:))
+}
+
+gugudan(start: 1, end: 9)
