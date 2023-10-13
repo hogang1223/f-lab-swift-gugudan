421fec41eda61c52623a3764336cbddc4e20c308
hogang <hyogangster1@gmail.com>
2023-10-13 18:46:31 +0900

    클래스로 구구단 구현

--- Gugudan/Gugudan/main.swift
+++ Gugudan/Gugudan/main.swift
@@ -17,3 +17,39 @@ import Foundation
  6. Functional Programming 형태로
  7. 깃허브에 올리고 코드 리뷰 요청하기
  */
+
+// 5. 클래스로
+
+struct Multiple {
+    let num1: Int
+    let num2: Int
+    var multipleNum: Int {
+        get {
+            return self.num1 * self.num2
+        }
+    }
+    func output() {
+        print("\(self.num1) X \(self.num2) = \(self.multipleNum)")
+    }
+}
+
+class Gugudan {
+    // 내가 이미 출력한 적이 있는 친구면 굳이 다시 계산할 필요가 없지
+    var dans = [Int: [Multiple]]()
+    func outputGugudan(column: Int) {
+        if let dan = dans[column] {
+            for multiple in dan {
+                multiple.output()
+            }
+        }
+        else {
+            var rows = [Multiple]()
+            for row in 1...9 {
+                let multiple = Multiple(num1: column, num2: row)
+                multiple.output()
+                rows.append(multiple)
+            }
+            dans[column] = rows
+        }
+    }
+}
